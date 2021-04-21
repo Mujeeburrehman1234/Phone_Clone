63e35bccf4e3b20a98f102c354e1cd3c048d6ad1
Mujeeburrehman1234 <mujeeb.rehman4422@gmail.com>
2021-04-21 16:08:33 +0500

    update version

---
+++ app/src/main/java/com/example/phone_clone/SerachNetwork.java
@@ -0,0 +1,14 @@
+package com.example.phone_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SerachNetwork extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serach_network);
+    }
+}
